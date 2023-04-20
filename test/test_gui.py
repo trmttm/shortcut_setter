883a2e7556ac362f9b9928e3f544bdeab7e33f26
trmttm <96355189+trmttm@users.noreply.github.com>
2023-04-20 14:11:42 +0900

    Popup

--- test/test_gui.py
+++ test/test_gui.py
@@ -4,12 +4,20 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_gui(self):
         from view_tkinter import View
+        from interface_tk import top_level_options
+        from interface_tk import widget_model
         app = View()
 
         import gui
         file_path = 'keyboard_shortcut.json'
         commands_to_short_cuts = gui.load_shortcut_configuration_file(file_path)
         n_commands = len(commands_to_short_cuts)
+
+        specified_parent = 'toplevel_root'
+        title = 'Filter setting'
+        width = 1000
+        height = 500
+        options = top_level_options(title, (width, height))
 
         def callback(command_str):
             print(command_str, gui.get_state(app, n_commands))
@@ -21,9 +29,10 @@ class MyTestCase(unittest.TestCase):
             elif command_str == gui.KEY_DONE:
                 data = dict(zip(commands_to_short_cuts.values(), gui.get_state(app, n_commands)))
                 gui.save_shortcut_configuration_file(file_path, data)
-                app.close('root')
+                app.close(specified_parent)
 
-        view_model = gui.create_view_model_of_shortcut_setter(callback, commands_to_short_cuts)
+        vm = [widget_model('root', specified_parent, 'toplevel', 0, 0, 0, 0, 'nswe', **options)]
+        view_model = vm + gui.create_view_model_of_shortcut_setter(callback, commands_to_short_cuts, specified_parent)
         app.add_widgets(view_model)
         [gui.bind_commands(n, app) for n in range(n_commands)]
         app.launch_app()
